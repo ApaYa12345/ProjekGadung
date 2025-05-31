015c2a040e75f5db558664ca0edc9952c89dafb9
ApaYa12345 <lamaxdang@gmail.com>
2025-06-01 00:07:33 +0700

    Add missing fetchCampuses import

--- src/components/campus/CampusSelector.tsx
+++ src/components/campus/CampusSelector.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { motion } from 'framer-motion';
-import { fetchCampusesByCity } from '../../lib/supabase';
+import { fetchCampuses, fetchCampusesByCity } from '../../lib/supabase';
 import { Campus } from '../../types';
 import Card from '../ui/Card';
 import Button from '../ui/Button';
